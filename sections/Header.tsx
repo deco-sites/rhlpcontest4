1713b8142c3588f06cd707e307d66575e7b41e36
Ricardo Henrique Gonzaga <ricardo.gonzaga@agenciam3.com>
2024-05-02 18:34:30 -0300

    feat: add header section

---
+++ sections/Header.tsx
@@ -0,0 +1,17 @@
+function Header() {
+    return (
+        <header class="bg-white border-b-solid border-b-2 border-b-black">
+            <section class="max-w-[1440px] flex items-center justify-between">
+                <span class="text-lg">Guia de turismo</span>
+                <div>
+                    <span class="text-sm">Sobre nós</span>
+                    <span class="text-sm">Oferta</span>
+                    <span class="text-sm">Contate-nos</span>
+                </div>
+            </section>
+        </header>
+    )
+
+}
+
+export default Header;
